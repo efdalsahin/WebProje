7e7cf86eaef21bfc482075c4fca519c884d8aceb
Efdal ŞAHİN <efdal.sahin60@gmail.com>
2023-11-25 14:00:00 +0100

    Uçus ve Ucak Controller

---
+++ SkyPioneer/Controllers/UcakController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SkyPioneer.Migrations;
+using SkyPioneer.Models;
+
+namespace SkyPioneer.Controllers
+{
+    public class UcakController : Controller
+    {
+        ApplicationDbContext context=new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UcakCreate()
+        {
+
+            var datafromDatabase = context.HavaYollari.ToList();
+
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            foreach (var item in datafromDatabase)
+            {
+                selectListItems.Add(new SelectListItem
+                {
+                    Text = item.Isim, // Göstermek istediğiniz alanın adı
+                    Value = item.HavaYoluID.ToString() // Seçilen değerin değeri
+                });
+            }
+            ViewBag.liste = selectListItems;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UcakCreate(Ucak ucak)
+        {
+            ModelState.Remove("HavaYolu");
+            ucak.HavaYolu=context.HavaYollari.Where(k=>k.HavaYoluID==ucak.HavaYoluID).FirstOrDefault();
+            if (ModelState.IsValid){ 
+                
+                context.Ucaklar.Add(ucak);
+                context.SaveChanges();
+                TempData["msj"] = ucak.UcakID+ " id li ucak oluşturuldu ";
+                return RedirectToAction("UcakCreate");
+
+            }
+            TempData["msj"] = ucak.UcakID + " id li ucak oluşturulamadı ";
+            return View();
+
+        }
+
+        public IActionResult UcakDelete(int id)
+        {
+            var dataFromDatabase = context.Ucaklar.Where(k => k.UcakID == id).FirstOrDefault();
+
+            dataFromDatabase.HavaYolu = context.HavaYollari.Where(k => k.HavaYoluID == dataFromDatabase.HavaYoluID).FirstOrDefault();
+           
+
+
+
+            return View(dataFromDatabase);
+
+        }
+
+        public IActionResult UcakDeletee(int id)
+        {
+            var dataFromDatabase = context.Ucaklar.Where(k => k.UcakID == id).FirstOrDefault();
+            int temp = dataFromDatabase.UcakID;
+            TempData["msj"] = temp + " id li uçak silindi ";
+
+            context.Ucaklar.Remove(dataFromDatabase);
+            context.SaveChanges();
+            return RedirectToAction("UcaklarDetails", "Admin");
+        }
+
+        public IActionResult UcakDetails(int id)
+        {
+            var dataFromDatabase = context.Ucaklar.Where(k => k.UcakID == id).FirstOrDefault();
+
+            dataFromDatabase.HavaYolu = context.HavaYollari.Where(k => k.HavaYoluID == dataFromDatabase.HavaYoluID).FirstOrDefault();
+
+            return View(dataFromDatabase);
+
+        }
+    }
+}
---
+++ SkyPioneer/Controllers/UcusController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SkyPioneer.Migrations;
+using SkyPioneer.Models;
+
+namespace SkyPioneer.Controllers
+{
+    public class UcusController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        // GET: UcusController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UcusController/Details/5
+        public ActionResult UcusDetails(int id)
+        {
+
+            var dataFromDatabase = context.Ucuslar.Where(k=> k.UcusID==id).FirstOrDefault();
+
+            dataFromDatabase.Kalkis=context.HavaAlanlari.Where(k => k.HavaAlaniID == dataFromDatabase.KalkisID).FirstOrDefault();
+            dataFromDatabase.Varis = context.HavaAlanlari.Where(k => k.HavaAlaniID == dataFromDatabase.VarisID).FirstOrDefault();
+            dataFromDatabase.Ucak = context.Ucaklar.Where(k => k.UcakID == dataFromDatabase.UcakID).FirstOrDefault();
+
+            
+
+            return View(dataFromDatabase);
+     
+        }
+
+        public ActionResult ToAirport(int id)
+        {
+            var datafromDatabase = context.HavaAlanlari.Where(x => x.HavaAlaniID != id).ToList();
+
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            foreach (var item in datafromDatabase)
+            {
+                selectListItems.Add(new SelectListItem
+                {
+                    Text = item.Isim, // Göstermek istediğiniz alanın adı
+                    Value = item.HavaAlaniID.ToString() // Seçilen değerin değeri
+                });
+            }
+            return Json(selectListItems);
+        }
+
+        // GET: UcusController/Create
+        public ActionResult UcusCreate()
+        {
+            var datafromDatabase = context.HavaAlanlari.ToList();
+
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            foreach (var item in datafromDatabase)
+            {
+                selectListItems.Add(new SelectListItem
+                {
+                    Text = item.Isim, // Göstermek istediğiniz alanın adı
+                    Value = item.HavaAlaniID.ToString() // Seçilen değerin değeri
+                });
+            }
+
+            var datafromDatabase2 = context.Ucaklar.ToList();
+
+            List<SelectListItem> selectListItems2 = new List<SelectListItem>();
+            foreach (var item in datafromDatabase2)
+            {
+                selectListItems2.Add(new SelectListItem
+                {
+                    Text = item.Modell, // Göstermek istediğiniz alanın adı
+                    Value = item.UcakID.ToString() // Seçilen değerin değeri
+                });
+            }
+
+
+
+
+            ViewBag.From = selectListItems;
+            ViewBag.Ucak = selectListItems2;
+            return View();
+           
+        }
+
+        // POST: UcusController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UcusCreate(Ucus ucus)
+        {
+            ModelState.Remove("Kalkis");
+            ModelState.Remove("Ucak");
+            ModelState.Remove("Varis");
+
+            if (ModelState.IsValid)
+            {
+                context.Ucuslar.Add(ucus);
+                context.SaveChanges();
+                TempData["msj"] = ucus.KalkisSaati + " saatli uçuş oluşturuldu ";
+                return RedirectToAction("UcusCreate");
+            }
+            TempData["msj"] = ucus.KalkisSaati + " saatli uçuş eklenemedi ";
+            return View();
+         
+        }
+
+
+        // GET: UcusController/Delete/5
+        public ActionResult UcusDelete(int id)
+        {
+            var dataFromDatabase = context.Ucuslar.Where(k => k.UcusID == id).FirstOrDefault();
+
+            dataFromDatabase.Kalkis = context.HavaAlanlari.Where(k => k.HavaAlaniID == dataFromDatabase.KalkisID).FirstOrDefault();
+            dataFromDatabase.Varis = context.HavaAlanlari.Where(k => k.HavaAlaniID == dataFromDatabase.VarisID).FirstOrDefault();
+            dataFromDatabase.Ucak = context.Ucaklar.Where(k => k.UcakID == dataFromDatabase.UcakID).FirstOrDefault();
+
+
+
+            return View(dataFromDatabase);
+            
+        }
+
+        // POST: UcusController/Delete/5
+        
+       
+        public ActionResult UcusDeletee(int id)
+        {
+            var dataFromDatabase = context.Ucuslar.Where(k => k.UcusID == id).FirstOrDefault();
+            int temp = dataFromDatabase.UcusID;
+            TempData["msj"] =temp+ " id li uçuş silindi ";
+
+            context.Ucuslar.Remove(dataFromDatabase);
+            context.SaveChanges();
+            return RedirectToAction("UcuslarDetails","Admin");
+        }
+    }
+}
